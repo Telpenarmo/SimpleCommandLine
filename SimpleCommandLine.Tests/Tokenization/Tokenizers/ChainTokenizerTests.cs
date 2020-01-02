96d5b962b32569cb9a35f60505ab219dd52ed260
Telpenarmo <telpenarmo@protonmail.com>
2020-01-02 12:30:30 +0100

    last christmas changes (tiny)

---
+++ SimpleCommandLine.Tests/Tokenization/Tokenizers/ChainTokenizerTests.cs
@@ -0,0 +1,25 @@
+using SimpleCommandLine.Tokenization.Tokenizers;
+using SimpleCommandLine.Tokenization.Tokens;
+using Xunit;
+
+namespace SimpleCommandLine.Tests.Tokenization.Tokenizers
+{
+    public class ChainTokenizerTests
+    {
+        private class FakeTokenizer : ChainTokenizer
+        {
+            public override bool CanHandle(string arg) => true;
+            public override IArgumentToken Handle(string arg) => null;
+        }
+
+        [Fact]
+        public void When_Next_set_to_non_ChainTokenizer_Add_inserts_into_middle()
+        {
+            var chain = new FakeTokenizer();
+            chain.Next = new ValueTokenizer();
+            chain.AddLink(new FakeTokenizer());
+            Assert.IsType<ChainTokenizer>(chain.Next);
+            Assert.IsType<ValueTokenizer>((chain.Next as ChainTokenizer).Next);
+        }
+    }
+}
